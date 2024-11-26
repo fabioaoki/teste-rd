4929345dfc14547178060678b111c0c97e1a6015
Fabio Aoki <fabio.aoki.93@gmail.com>
2024-11-25 22:41:18 -0300

    Initial commit.

---
+++ src/main/java/com/teste/rd/Teste/RD/TesteRdApplication.java
@@ -0,0 +1,13 @@
+package com.teste.rd.Teste.RD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteRdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteRdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/rd/Teste/RD/TesteRdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.rd.Teste.RD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteRdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
